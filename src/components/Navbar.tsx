2b24f4c11eb46895fa717293d6b56edadc81ed9a
jaswanthreddy-007 <88081034+jaswanthreddy-007@users.noreply.github.com>
2024-03-08 21:14:49 +0530

    logo fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -52,11 +52,11 @@ export const Navbar = () => {
     <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
       <NavigationMenu className="mx-auto">
         <NavigationMenuList className="container h-14 px-4 w-screen flex justify-between ">
-          <NavigationMenuItem className="font-bold flex justify-center">
-            <a href="/" className="ml-2 font-bold flex">
-              <img src={logo} alt="LearnSync Logo" className="h-20 w-20 p-0" />
+          <NavigationMenuItem className="font-bold flex">
+            <a href="/" className="ml-2 font-bold text-xl flex">
+              <LogoIcon />
+              LearnSync
             </a>
-            <p>LearnSync</p>
           </NavigationMenuItem>
 
           {/* desktop */}
